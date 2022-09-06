836e90a0f1445170ee4db920202a5ac866e6dcd2
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-06 11:08:11 -0700

    Time: 17 ms (16.23%), Space: 45.3 MB (48.12%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals == null || intervals.length==0) return 0;
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int[] interval : intervals){
+            if(!pq.isEmpty() && pq.peek()<=interval[0])
+                pq.poll();
+            pq.add(interval[1]);
+        }
+        return pq.size();
+    }
+}

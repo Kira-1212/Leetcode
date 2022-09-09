dd3f6a6764c0c6afd334822dbd34bbb54a058259
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-08 17:21:25 -0700

    Time: 4 ms (63.66%), Space: 68 MB (83.98%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] outdegrees = new int[n+1];
+
         int[] indegrees = new int[n+1];
         for(int[] t : trust){
-            outdegrees[t[0]]++;
+            indegrees[t[0]]--;
             indegrees[t[1]]++;
         }
         int result =-1;
         for(int i = 1; i<=n; i++){
-            if(outdegrees[i]==0)
+
                 if(indegrees[i] == n-1)
                     result = i;
             }

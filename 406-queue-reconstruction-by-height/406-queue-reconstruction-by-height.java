be6be1bdcf900d4bdd69cfa8a519ba7ad6552ef1
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-14 13:07:41 -0700

    Time: 22 ms (17.61%), Space: 54.4 MB (70.44%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        if(people == null || people.length == 0) return new int [][]{{}};
+        Arrays.sort(people, (a,b) ->{
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }
+            return b[0]-a[0];
+        });
+        List<int[]> result = new ArrayList<>();
+        for(int[] p : people){
+            result.add(p[1],p);
+        }
+        return result.toArray(new int[0][]);
+    }
+}

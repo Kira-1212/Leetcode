3d0e071ad4d41dc49dac9b979411923274eae3f9
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-18 10:24:44 -0700

    Time: 14 ms (23.08%), Space: 89.3 MB (70.66%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -2,26 +2,26 @@ class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
         if(tops == null || tops.length ==0) return 0;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int target = -1;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int target = -1;
         
-        for(int i = 0; i<tops.length; i++){
-            map.put(tops[i], map.getOrDefault(tops[i], 0)+1);
-            map.put(bottoms[i], map.getOrDefault(bottoms[i], 0)+1);
-            if(map.get(tops[i]) >= tops.length){
-                target = tops[i];
-                break;
-            }
-            if(map.get(bottoms[i]) >= tops.length){
-                target = bottoms[i];
-                break;
-            }
+//         for(int i = 0; i<tops.length; i++){
+//             map.put(tops[i], map.getOrDefault(tops[i], 0)+1);
+//             map.put(bottoms[i], map.getOrDefault(bottoms[i], 0)+1);
+//             if(map.get(tops[i]) >= tops.length){
+//                 target = tops[i];
+//                 break;
+//             }
+//             if(map.get(bottoms[i]) >= tops.length){
+//                 target = bottoms[i];
+//                 break;
+//             }
             
-        }
-        if(target == -1)
+//         }
+        int target = countRotations(tops, bottoms, tops[0]);
+        if(target != -1)
             return target;
-        
-        return countRotations(tops, bottoms, target);
+        return countRotations(tops, bottoms, bottoms[0]);
     }
     private int countRotations(int[] tops, int[] bottoms, int target){
         int topcount = 0;

6cee022e4fb370f20693c9b024a672442b8b734b
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-18 10:22:51 -0700

    Time: 46 ms (11.73%), Space: 49.7 MB (97.39%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        if(tops == null || tops.length ==0) return 0;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int target = -1;
+        
+        for(int i = 0; i<tops.length; i++){
+            map.put(tops[i], map.getOrDefault(tops[i], 0)+1);
+            map.put(bottoms[i], map.getOrDefault(bottoms[i], 0)+1);
+            if(map.get(tops[i]) >= tops.length){
+                target = tops[i];
+                break;
+            }
+            if(map.get(bottoms[i]) >= tops.length){
+                target = bottoms[i];
+                break;
+            }
+            
+        }
+        if(target == -1)
+            return target;
+        
+        return countRotations(tops, bottoms, target);
+    }
+    private int countRotations(int[] tops, int[] bottoms, int target){
+        int topcount = 0;
+        int bottomcount =0;
+        for( int i = 0; i<tops.length; i++){
+            if(tops[i] != target && bottoms[i] != target)
+                return -1;
+            if(tops[i] != target)
+                topcount++;
+            if(bottoms[i] != target)
+                bottomcount++;
+        }
+        return Math.min(topcount, bottomcount);
+    }
+}

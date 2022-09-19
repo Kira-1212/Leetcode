c10099e65a94d83282ea5697487637794c279556
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-19 16:23:58 -0700

    Time: 28 ms (56.67%), Space: 54.5 MB (23.07%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -1,24 +1,45 @@
 /* The knows API is defined in the parent class Relation.
       boolean knows(int a, int b); */
 
+// public class Solution extends Relation {
+//     public int findCelebrity(int n) {
+//         if(n == 0 )return -1;
+        
+//         int[] indegrees = new int[n];
+//         for( int i =0; i<n; i++){
+//             for( int j =0; j<n ; j++){
+//                 if( i== j) continue;
+//                 if(knows(i, j)){
+//                     indegrees[i]--;
+//                     indegrees[j]++;
+//                 }
+//             }
+//         }
+//         for( int i = 0; i<n; i++){
+//             if(indegrees[i] == n-1)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
+
+
 public class Solution extends Relation {
     public int findCelebrity(int n) {
         if(n == 0 )return -1;
+        int celeb = 0;
         
-        int[] indegrees = new int[n];
-        for( int i =0; i<n; i++){
-            for( int j =0; j<n ; j++){
-                if( i== j) continue;
-                if(knows(i, j)){
-                    indegrees[i]--;
-                    indegrees[j]++;
-                }
-            }
+        for( int i =1; i<n; i++){
+            if(knows(celeb, i))
+                celeb =i;
         }
+        
+        
         for( int i = 0; i<n; i++){
-            if(indegrees[i] == n-1)
-                return i;
+            if(i == celeb)continue;
+            if(!knows(i, celeb) || knows(celeb, i) )
+                return -1;
         }
-        return -1;
+        return celeb;
     }
 }

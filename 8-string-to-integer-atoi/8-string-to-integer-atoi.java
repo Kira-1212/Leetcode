39ff4b2366a6f02a0a24940e156f68f0010f38ac
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-21 19:59:26 -0700

    Time: 5 ms (28.15%), Space: 43.3 MB (26.63%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int myAtoi(String s) {
+        if(s == null || s.length() == 0)return 0;
+        s = s.trim();
+        if(s.length() == 0)return 0;
+        char sign ='+';
+        char first = s.charAt(0);
+        int result =0;
+        int limit = Integer.MAX_VALUE/10;
+        if(first == '-')
+            sign  = '-';
+        if(s.charAt(0)!='+' && sign != '-' && !Character.isDigit(first)) return 0;
+        for(int i =0; i<s.length(); i++ ){
+            char c= s.charAt(i);
+            if(Character.isDigit(c)){
+                if(sign == '+'){
+                    if(result > limit) return Integer.MAX_VALUE;
+                    else if(result == limit){
+                        if(c-'0'>7){
+                            return Integer.MAX_VALUE;
+                        }
+                    }
+                }
+                else{
+                    if(result > limit) return Integer.MIN_VALUE;
+                    else if(result == limit){
+                        if(c-'0'>8){
+                            return Integer.MIN_VALUE;
+                        }
+                    }
+                    
+                }
+                result = result * 10 + (c-'0');
+            }
+            else if(i!=0){
+                break;
+            }
+            
+            
+            
+            
+        }
+        if(sign == '-')
+            return -result;
+        return result;
+    }
+}

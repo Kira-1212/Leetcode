7b85436cac97a53eacaf90fbd15bb69cbe1bf5d7
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-09-29 14:57:20 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (49.05%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,8 +1,27 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         if(s == null)return 0;
+//         s= s.trim();
+//         String[] str = s.split(" ");
+//         return str[str.length-1].length();
+//     }
+// }
+
+
 class Solution {
     public int lengthOfLastWord(String s) {
         if(s == null)return 0;
-        s= s.trim();
-        String[] str = s.split(" ");
-        return str[str.length-1].length();
+        int n = s.length();
+        int length = 0;
+        while(n>0){
+            n--;
+            if(s.charAt(n)!=' '){
+                length++;
+            }
+            else if(length>0)
+                return length;
+        }
+        
+        return length;
     }
 }

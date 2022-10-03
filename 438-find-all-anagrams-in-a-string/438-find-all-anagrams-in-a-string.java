6e06b202327a921da867f875938ff90e893ee8e0
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-03 13:20:01 -0700

    Time: 100 ms (24.19%), Space: 48.5 MB (34.98%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,37 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        if(s == null || p == null || s.length() == 0 || p.length() ==0)return new ArrayList<>();
-        
-        HashMap<Character, Integer> map = new HashMap();
-        List<Integer> result = new ArrayList<>();
-        for(int i =0; i<p.length(); i++){
-            char c = p.charAt(i);
-            map.put(c, map.getOrDefault(c, 0)+1);
+        List<Integer> result = new ArrayList<Integer>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int c = 0;
+        for(int i = 0; i<p.length(); i++){
+            map.put(p.charAt(i), map.getOrDefault(p.charAt(i), 0) + 1);
         }
-        int match = 0;
-        for(int i = 0; i<s.length(); i++){
+        int i = 0;
+        while(i<s.length()){
             char in = s.charAt(i);
             if(map.containsKey(in)){
                 int count = map.get(in);
                 count--;
-                 map.put(in, count);
+                map.put(in, count);
                 if(count == 0)
-                    match++;
-               
+                    c++;
             }
+            
             if(i>=p.length()){
-                char out = s.charAt(i-p.length());
+                char out = s.charAt(i -p.length());
                 if(map.containsKey(out)){
                     int count = map.get(out);
                     count++;
                     map.put(out, count);
                     if(count == 1)
-                        match--;
+                        c--;
                 }
             }
-            if(match == map.size()){
-                result.add(i-p.length()+1);
+            if(c==map.size()){
+                result.add(i+1-p.length());
             }
+            i++;
+            
         }
         return result;
     }

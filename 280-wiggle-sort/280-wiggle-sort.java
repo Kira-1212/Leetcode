6330c5a36ac7ca3124b431f261831bea2eab41dd
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-06 19:48:54 -0700

    Time: 9 ms (5.69%), Space: 49.4 MB (31.30%) - LeetHub

--- 280-wiggle-sort/280-wiggle-sort.java
+++ 280-wiggle-sort/280-wiggle-sort.java
@@ -3,23 +3,13 @@ class Solution {
         if(nums.length<2)return ;
         Arrays.sort(nums);
         
-        int[] result = new int[nums.length];
-        
-        int i =0;
-        int l = 0;
-        int r = nums.length-1;
-        
-        while(l<=r && i<nums.length){
-            result[i] = nums[l];
-            l++;
-            i++;
-            if(i<nums.length){
-                result[i] = nums[r];
-                i++;
-                r--;
-            }
+        for(int i = 1; i<nums.length-1; i+=2){
+            swap(nums, i, i+1);
         }
-        for(i = 0; i<nums.length; i++)
-            nums[i]=result[i];
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j]= temp;
     }
 }

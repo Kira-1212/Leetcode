69476e856888fcfb744b928420a9911149ab6982
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-10 19:46:49 -0700

    Time: 1775 ms (5.00%), Space: 47.3 MB (85.27%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,30 @@
 class Solution {
-    List<List<Integer>> result = new ArrayList<>();
+    Set<List<Integer>> result = new HashSet<>();
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         
         for(int i = 0; i<nums.length; i++){
-            if(i == 0 || nums[i-1] != nums[i] )
-                twoSum(nums, i);
+            twoSum(nums, i);
         }
-        return result;
+        return new ArrayList(result);
     }
     
     private void twoSum(int[] nums, int i){
         List<Integer> res = new ArrayList<>();
-        int l = i +1;
-        int r = nums.length -1;
-        while(l<r){
-            
-            int sum = nums[i] + nums[l] + nums[r];
-            if(sum<0)
-                l++;
-            else if(sum>0)
-                r--;
-            else{
-                result.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                l++;
-                r--;
-                while(l<r && nums[l] == nums[l-1])
-                    ++l;
+       
+        int target = -nums[i];
+        HashMap<Integer,Integer> map= new HashMap<>();
+        for(int j = i+1; j<nums.length; j++){
+            int comp = target - nums[j];
+            if(map.containsKey(comp) && map.get(comp) == i){
+                List<Integer> triplet = Arrays.asList(nums[i], nums[j], comp);
+                Collections.sort(triplet);
+                result.add(triplet);
             }
-            
+            else{
+                map.put(nums[j], i);
+            }
         }
+        
     }
 }

74b4d92977fa82feb9eb6eaa5a1e18042468deaa
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-17 12:06:41 -0700

    Time: 1 ms (94.09%), Space: 43.1 MB (76.60%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,31 +13,54 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         if(root == null)return new ArrayList<>();
+//         List<List<Integer>> result = new ArrayList<>();
+        
+//         Queue<TreeNode> q = new LinkedList<>();
+        
+//         q.add(root);
+        
+        
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             List<Integer> r = new ArrayList<>();
+//             for( int i = 0; i<size; i++){
+                
+//                 TreeNode curr = q.poll();
+//                 r.add(curr.val);
+//                 if(curr.left!=null)
+//                     q.add(curr.left);
+//                 if(curr.right!=null)
+//                     q.add(curr.right);
+//             }
+//             result.add(r);
+            
+//         }
+//         return result;
+//     }
+// }
+
+
 class Solution {
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root == null)return new ArrayList<>();
-        List<List<Integer>> result = new ArrayList<>();
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        q.add(root);
+        dfs(root, 0);
         
         
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> r = new ArrayList<>();
-            for( int i = 0; i<size; i++){
-                
-                TreeNode curr = q.poll();
-                r.add(curr.val);
-                if(curr.left!=null)
-                    q.add(curr.left);
-                if(curr.right!=null)
-                    q.add(curr.right);
-            }
-            result.add(r);
-            
-        }
         return result;
+    }
+    
+    private void dfs(TreeNode root, int level){
+        if(root == null)return;
+        
+        if(level == result.size()){
+            result.add(new ArrayList<>());
+        }
+        result.get(level).add(root.val);
+        dfs(root.left, level + 1);
+        dfs(root.right, level +1);
     }
 }

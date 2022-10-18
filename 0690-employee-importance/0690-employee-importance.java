b708871561fb8d9aca21b4141070754a89033ed2
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-18 15:16:35 -0700

    Time: 12 ms (33.73%), Space: 57.9 MB (69.02%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.java
+++ 0690-employee-importance/0690-employee-importance.java
@@ -9,18 +9,35 @@ class Employee {
 
 class Solution {
     Map<Integer, Employee> map;
-    public int getImportance(List<Employee> employees, int id) {
+    // public int getImportance(List<Employee> employees, int id) {
+    //     map = new HashMap<>();
+    //     for(Employee e: employees){
+    //         map.put(e.id, e);
+    //     }
+    //     return helper(id);
+    // }
+    // private int helper(int id){
+    //     int result = map.get(id).importance;
+    //     for(int sub  : map.get(id).subordinates){
+    //         result += helper(sub);
+    //     }
+    //     return result;
+    // }
+    
+     public int getImportance(List<Employee> employees, int id) {
         map = new HashMap<>();
         for(Employee e: employees){
             map.put(e.id, e);
         }
-        return helper(id);
-    }
-    private int helper(int id){
-        int result = map.get(id).importance;
-        for(int sub  : map.get(id).subordinates){
-            result += helper(sub);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(id);
+        int total = 0;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            total += map.get(curr).importance;
+            for(int sub : map.get(curr).subordinates)
+                q.add(sub);
         }
-        return result;
+         return total;
     }
 }

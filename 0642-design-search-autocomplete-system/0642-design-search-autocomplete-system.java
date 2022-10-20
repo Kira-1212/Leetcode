67e347d9d22ecdf9eb4e6deeaab3b09103cc65f4
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-19 17:16:37 -0700

    Time: 405 ms (24.30%), Space: 119.4 MB (72.36%) - LeetHub

--- 0642-design-search-autocomplete-system/0642-design-search-autocomplete-system.java
+++ 0642-design-search-autocomplete-system/0642-design-search-autocomplete-system.java
@@ -1,24 +1,59 @@
 class AutocompleteSystem {
+    class TrieNode {
+        HashMap<String, Integer> map;
+        HashMap<Character, TrieNode> children;
 
-    HashMap<String, Integer> map;
+        public TrieNode() {
+            map = new HashMap<>();
+            children = new HashMap<>();
+
+        }
+    }
+
+    TrieNode root;
     StringBuilder input;
 
+    public void insert(String sentence, int times) {
+        TrieNode curr = root;
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                curr.children.put(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+            curr.map.put(sentence, curr.map.getOrDefault(sentence, 0) + times);
+        }
+    }
+
+    public HashMap<String, Integer> search(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (!curr.children.containsKey(c)) {
+                return new HashMap<>();
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.map;
+    }
+
     public AutocompleteSystem(String[] sentences, int[] times) {
-        map = new HashMap<>();
+        root = new TrieNode();
         input = new StringBuilder();
         for (int i = 0; i < sentences.length; i++) {
-            map.put(sentences[i], map.getOrDefault(sentences[i], 0) + times[i]);
+            insert(sentences[i], times[i]);
         }
     }
 
     public List<String> input(char c) {
         if (c == '#') {
             String in = input.toString();
-            map.put(in, map.getOrDefault(in, 0) + 1);
+            insert(in, 1);
             input = new StringBuilder();
             return new ArrayList<>();
         }
         input.append(c);
+        HashMap<String, Integer> map = search(input.toString());
         PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> {
             if (map.get(a) == map.get(b)) {
                 return b.compareTo(a);

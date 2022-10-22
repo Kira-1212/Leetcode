d7ea5780ac616b7bdf954ca1cfe38d4f256fe0dd
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-10-22 10:55:06 -0700

    Time: 1 ms (46.93%), Space: 42.1 MB (57.20%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,21 +13,67 @@
  *     }
  * }
  */
-class Solution {
-    List<Integer> result = new ArrayList<>();
+// class Solution {
+//     List<Integer> result = new ArrayList<>();
     
-    public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return result;
-        dfs(root);
-        return result;
-    }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root == null) return result;
+//         dfs(root);
+//         return result;
+//     }
     
-    private void dfs(TreeNode root){
-        if(root == null)
-            return;
+//     private void dfs(TreeNode root){
+//         if(root == null)
+//             return;
         
-        dfs(root.left);
-        result.add(root.val);
-        dfs(root.right);
+//         dfs(root.left);
+//         result.add(root.val);
+//         dfs(root.right);
+//     }
+// }
+
+// class Solution {
+//     List<Integer> result = new ArrayList<>();
+    
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         if(root == null) return result;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         while(!q.isEmpty()){
+            
+//         }
+//         return result;
+//     }
+    
+    
+// }
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.left == null){
+                res.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode pre = curr.left;
+                while(pre.right != curr && pre.right != null){
+                    pre = pre.right;
+                }
+                if(pre.right == null){
+                    pre.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    pre.right = null;
+                    res.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return res;
     }
 }

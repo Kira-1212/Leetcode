b4c5df53a7d2941177ad657c10cb33a00ef1e821
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2022-11-20 18:18:41 -0800

    Time: 245 ms (54.47%), Space: 159.5 MB (24.26%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,47 +1,33 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
-        
-        // Sort based on min task processing time or min task index.
-        // Store enqueue time, processing time, task index.
-        PriorityQueue<int[]> nextTask = new PriorityQueue<int[]>((a, b) -> (a[1] != b[1] ? (a[1] - b[1]) : (a[2] - b[2])));
-        
-        // Store task enqueue time, processing time, index.
-        int sortedTasks[][] = new int[tasks.length][3];
-        for (int i = 0; i < tasks.length; ++i) {
-            sortedTasks[i][0] = tasks[i][0];
-            sortedTasks[i][1] = tasks[i][1];
-            sortedTasks[i][2] = i;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> (a[1] != b[1] ? (a[1] - b[1]) : (a[2] - b[2])));
+        int[][] sorted = new int[tasks.length][3];
+        for(int i = 0; i<tasks.length; i++){
+            sorted[i][0] = tasks[i][0];
+            sorted[i][1] = tasks[i][1];
+            sorted[i][2] = i;
         }
-        
-        Arrays.sort(sortedTasks, (a, b) -> Integer.compare(a[0], b[0]));
-        int tasksProcessingOrder[] = new int[tasks.length];
-        
-        long currTime = 0;
-        int taskIndex = 0;
+        int res[] = new int[tasks.length];
+        Arrays.sort(sorted, (a,b) -> a[0] - b[0]);
+        int i = 0;
+        int currTime = 0;
         int ansIndex = 0;
+        while(i<tasks.length || !pq.isEmpty()){
+            if(pq.isEmpty() && currTime<sorted[i][0])
+                currTime=sorted[i][0];
         
-        // Stop when no tasks are left in array and heap.
-        while (taskIndex < tasks.length || !nextTask.isEmpty()) {
-            if (nextTask.isEmpty() && currTime < sortedTasks[taskIndex][0]) {
-                // When the heap is empty, try updating currTime to next task's enqueue time. 
-                currTime = sortedTasks[taskIndex][0];
+            while(i<tasks.length && currTime >= sorted[i][0]){
+                pq.add(sorted[i]);
+                i++;
             }
-            
-            // Push all the tasks whose enqueueTime <= currtTime into the heap.
-            while (taskIndex < tasks.length && currTime >= sortedTasks[taskIndex][0]) { 
-                nextTask.add(sortedTasks[taskIndex]);
-                ++taskIndex;
-            }
-            
-            int processTime = nextTask.peek()[1];
-            int index = nextTask.peek()[2];
-            nextTask.remove();
-            
-            // Complete this task and increment currTime.
-            currTime += processTime; 
-            tasksProcessingOrder[ansIndex++] = index;
+            int procTime = pq.peek()[1];
+            int index = pq.peek()[2];
+            currTime += procTime;
+            pq.poll();
+            res[ansIndex]=index;
+            ansIndex++;
         }
-        
-        return tasksProcessingOrder;
+        return res;
     }
+    
 }

0aabc8df5d8c606b281533e084f7fa8c53ebe82d
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-07-09 19:13:02 -0400

    Time: 8 ms (65.03%), Space: 46.2 MB (23.36%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,15 +1,12 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) ->a[0]-b[0]);
+        if(intervals== null ||intervals.length == 0)return 0;
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int[] i : intervals){
-            if(!pq.isEmpty() && pq.peek()<=i[0]){
-                
-                    pq.poll();
-                
-            }
-            pq.add(i[1]);
+        for(int[] a : intervals){
+            if(!pq.isEmpty() && pq.peek()<=a[0])
+                pq.poll();
+            pq.add(a[1]);
         }
         return pq.size();
     }

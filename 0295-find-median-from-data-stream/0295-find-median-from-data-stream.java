d1aa43ca30669be3579c58735a8866f8dc330f3c
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-07-10 17:32:23 -0400

    Time: 92 ms (78.88%), Space: 63.5 MB (59.35%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,37 +1,27 @@
 class MedianFinder {
     PriorityQueue<Integer> small;
     PriorityQueue<Integer> large;
-    
-    
     public MedianFinder() {
-        small = new PriorityQueue<>((a,b)-> b-a);
+        small = new PriorityQueue<>((a,b) -> b-a);
         large = new PriorityQueue<>();
     }
     
     public void addNum(int num) {
         small.add(num);
-        if(!small.isEmpty() && !large.isEmpty() && small.peek()>large.peek()){
+        if(!small.isEmpty() && !large.isEmpty() && small.peek() > large.peek())
             large.add(small.poll());
-        }
-        if(small.size() > large.size() + 1){
+        if(small.size() > large.size() + 1)
             large.add(small.poll());
-        }
-        if(large.size() > small.size() + 1 ){
+        if(large.size() > small.size() +1)
             small.add(large.poll());
-        }
     }
     
     public double findMedian() {
-        if(small.size() > large.size()){
-            return (double)small.peek();
-        }
-         if(small.size() < large.size()){
+        if(small.size() < large.size())
             return (double)large.peek();
-        }
-        
-            return (double)(small.peek() + large.peek())/2;
-        
-        
+        if(small.size() > large.size())
+            return (double)small.peek();
+        return ((double)small.peek() + (double)large.peek() )/2;
     }
 }
 

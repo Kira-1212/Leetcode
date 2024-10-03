a58777722b4602d681cb3bcf09d5d099b2270400
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-10-03 10:42:58 -0400

    Time: 17 ms (79.13%), Space: 45.7 MB (24.47%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.java
@@ -1,30 +1,62 @@
+// class Solution {
+//     public String minRemoveToMakeValid(String s) {
+//         Set<Integer> set = new HashSet<Integer>();
+//         Stack<Integer> st = new Stack<>();
+
+//         for(int i = 0; i<s.length(); i++){
+//             char c = s.charAt(i);
+//             if(c == '(')
+//                 st.push(i);
+//             if(c == ')'){
+//                 if(st.isEmpty()){
+//                     set.add(i);
+//                 }
+//                 else{
+//                     st.pop();
+//                 }
+//             }
+
+//         }
+//         while(!st.isEmpty())
+//             set.add(st.pop());
+//         StringBuilder sb = new StringBuilder();
+
+//         for(int i = 0; i<s.length(); i++){
+//             if(!set.contains(i))
+//                 sb.append(s.charAt(i));
+//         }
+//         return sb.toString();
+//     }
+// }
+
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Set<Integer> set = new HashSet<Integer>();
-        Stack<Integer> st = new Stack<>();
-
+        int openSeen = 0;
+        int balance = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c == '(')
-                st.push(i);
-            if(c == ')'){
-                if(st.isEmpty()){
-                    set.add(i);
-                }
-                else{
-                    st.pop();
-                }
+            if(c == '('){
+                openSeen++;
+                balance++;
             }
-
+            if(c==')'){
+                if(balance == 0)
+                    continue;
+                balance--;
+            }
+            sb.append(c);
         }
-        while(!st.isEmpty())
-            set.add(st.pop());
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i<s.length(); i++){
-            if(!set.contains(i))
-                sb.append(s.charAt(i));
+        StringBuilder result = new StringBuilder();
+        int openToKeep = openSeen - balance;
+        for(int i = 0; i< sb.length(); i++){
+            char c= sb.charAt(i);
+            if(c == '('){
+                openToKeep--;
+                if(openToKeep < 0)continue;
+            }
+            result.append(c);
         }
-        return sb.toString();
+        return result.toString();
     }
 }

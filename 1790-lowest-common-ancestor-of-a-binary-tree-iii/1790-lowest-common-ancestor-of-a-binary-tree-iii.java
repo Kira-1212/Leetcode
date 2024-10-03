1c2f211e5c802b591891fdff1c30369ffe830e7b
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-10-03 12:16:25 -0400

    Time: 30 ms (34.87%), Space: 44.9 MB (20.38%) - LeetHub

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -8,18 +8,31 @@ class Node {
 };
 */
 
+// class Solution {
+//     public Node lowestCommonAncestor(Node p, Node q) {
+//         Set<Node> set = new HashSet<>();
+//         while(p!= null){
+//             set.add(p);
+//             p =p.parent;
+//         }
+//         while(q!=null){
+//             if(set.contains(q))
+//             return q;
+//             q= q.parent;
+//         }
+//         return null;
+//     }
+// }
+
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
-        Set<Node> set = new HashSet<>();
-        while(p!= null){
-            set.add(p);
-            p =p.parent;
+        Node a =p;
+        Node b = q;
+        while(a!= b){
+            a = a == null? q: a.parent;
+            b = b== null? p: b.parent;
         }
-        while(q!=null){
-            if(set.contains(q))
-            return q;
-            q= q.parent;
-        }
-        return null;
+
+        return a;
     }
 }

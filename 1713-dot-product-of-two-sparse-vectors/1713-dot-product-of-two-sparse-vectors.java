b4f1111879a014339035e7e1cca1a5ea69d70f5c
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-10-03 15:31:00 -0400

    Time: 2 ms (100%), Space: 56.8 MB (89.4%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.java
@@ -1,26 +1,17 @@
 class SparseVector {
-    // Map the index to value for all non-zero values in the vector
-    Map<Integer, Integer> mapping;      
+
+    private int[] array;
 
     SparseVector(int[] nums) {
-        mapping = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            if (nums[i] != 0) {
-                mapping.put(i, nums[i]);        
-            }
-        }
+        array = nums;
     }
 
-    public int dotProduct(SparseVector vec) {        
+    public int dotProduct(SparseVector vec) {
         int result = 0;
 
-        // iterate through each non-zero element in this sparse vector
-        // update the dot product if the corresponding index has a non-zero value in the other vector
-        for (Integer i : this.mapping.keySet()) {
-            if (vec.mapping.containsKey(i)) {
-                result += this.mapping.get(i) * vec.mapping.get(i);
-            }
+        for (int i = 0; i < array.length; i++) {
+            result += array[i] * vec.array[i];
         }
         return result;
-    }
+    }   
 }

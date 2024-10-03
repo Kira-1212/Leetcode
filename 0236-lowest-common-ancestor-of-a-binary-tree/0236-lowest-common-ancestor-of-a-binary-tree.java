1080c4480ccec209be720a48cb588c55780f1e90
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-10-03 11:51:59 -0400

    Time: 7 ms (62.26%), Space: 44.8 MB (44.78%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,31 +1,41 @@
 class Solution {
-    List<TreeNode> pathP, pathQ;
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null )return root;
 
-        backtrack(root, p, q, new ArrayList<>());
-        for(int i = 0; i<pathP.size(); i++){
-            if(pathP.get(i) != pathQ.get(i))
-                return pathP.get(i-1);
-        }
-        return null;
-        
-        
+    private TreeNode ans;
+
+    public Solution() {
+        // Variable to store LCA node.
+        this.ans = null;
     }
-    private void backtrack(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> path){
-        if(root == null) return;
-        path.add(root);
-        if(root== p){
-            pathP= new ArrayList<>(path);
-            pathP.add(p);
+
+    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+        // If reached the end of a branch, return false.
+        if (currentNode == null) {
+            return false;
         }
-        if(root== q){
-            pathQ= new ArrayList<>(path);
-            pathQ.add(q);
+
+        // Left Recursion. If left recursion returns true, set left = 1 else 0
+        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+        // Right Recursion
+        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+        // If the current node is one of p or q
+        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+
+        // If any two of the flags left, right or mid become True
+        if (mid + left + right >= 2) {
+            this.ans = currentNode;
         }
-        backtrack(root.left, p, q, path);
-        backtrack(root.right, p, q, path);
-        path.remove(path.size()-1);
-        
+
+        // Return true if any one of the three bool values is True.
+        return (mid + left + right > 0);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Traverse the tree
+        this.recurseTree(root, p, q);
+        return this.ans;
     }
 }

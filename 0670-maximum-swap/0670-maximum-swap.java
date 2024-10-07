cb647c12e5fa37b4fae7be8662ebc66051ad14a2
Sai Kiran Davuluri <saikiran.davuluri@gmail.com>
2024-10-07 11:36:46 -0400

    Time: 0 ms (100%), Space: 40.2 MB (80.15%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,25 +1,25 @@
 class Solution {
     public int maximumSwap(int num) {
-        String s=String.valueOf(num);
-        char a[]=s.toCharArray();
-        int f[]=new int[s.length()];
-        int i=s.length()-1;
-        f[i]=i;
-        for(int k=s.length()-2;k>=0;k--){
-            if(a[i]<a[k])
-                i=k;
-            f[k]=i;
+        String str = String.valueOf(num);
+        char[] s = str.toCharArray();
+
+        int[] max = new int[s.length];
+        int i = s.length-1;
+        max[i] = i;
+        for(int k = s.length-2; k>=0; k--){
+            if(s[i] < s[k])
+                i = k;
+            max[k] = i;
         }
-        for(int j=0;j<s.length();j++){
-            if(a[j]<a[f[j]]){
-                char t=a[j];
-                a[j]=a[f[j]];
-                a[f[j]]=t;
+
+        for(int j = 0; j<s.length; j++){
+            if(s[j] < s[max[j]]){
+                char temp = s[j];
+                s[j] = s[max[j]];
+                s[max[j]] = temp;
                 break;
             }
         }
-        
-        return Integer.parseInt(new String(a));
-    
+        return Integer.parseInt(new String(s));
     }
 }
